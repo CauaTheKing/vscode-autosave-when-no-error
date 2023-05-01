fafd870e051ec5a456f9de7ce25b903c6a5a300a
CauaTheKing <cauascp37@gmail.com>
2023-05-01 10:34:57 -0300

    on window change added

--- extension.js
+++ extension.js
@@ -1,24 +1,73 @@
+const vscode = require("vscode");
 const { handleDocumentEdit } = require("./src/documentManager");
 const { track, TRACKED_ACTIONS } = require("./src/tracker");
+const fs = require("fs");
+
+const configs = vscode.workspace.getConfiguration("cau-vscode-autosave");
 
 var globalContext = null;
 /**
  * @param {vscode.ExtensionContext} context
  */
 async function activate(context) {
-  globalContext = context;
-  handleDocumentEdit(context);
-  track(context, TRACKED_ACTIONS.ACTIVATE, {
-    at: new Date().toUTCString(),
-  });
+    let windowsBlacklistString = `${configs.get("windowsBlacklist")}`;
+
+    windowsBlacklistString = windowsBlacklistString.toUpperCase();
+    windowsBlacklistString = windowsBlacklistString.replaceAll('\\"', "§dbl_quot§");
+    windowsBlacklistString = windowsBlacklistString.replaceAll('", ', '",');
+    if (windowsBlacklistString.endsWith(",") && windowsBlacklistString !== '"CODE",') {
+        windowsBlacklistString = windowsBlacklistString.substring(0, windowsBlacklistString.length - 1);
+    }
+    if (windowsBlacklistString.endsWith('"')) {
+        windowsBlacklistString = windowsBlacklistString.substring(0, windowsBlacklistString.length - 1);
+    }
+
+    let windowsBlacklistArray = windowsBlacklistString.split('",');
+    windowsBlacklistString = windowsBlacklistString.replaceAll('",', '", ');
+    windowsBlacklistString = windowsBlacklistString.replaceAll("§dbl_quot§", '\\"');
+
+    windowsBlacklistArray = JSON.parse(
+        JSON.stringify(windowsBlacklistArray)
+            .replaceAll('\\"', "")
+            .replaceAll("§dbl_quot§", String("\\" + '"'))
+    );
+
+    try {
+        fs.writeFile("./windows_blacklist.json", JSON.stringify(windowsBlacklistArray, null, 4));
+    } catch (error) {
+        console.warn(error);
+    }
+
+    windowsBlacklistString = windowsBlacklistString + '"';
+
+    if (windowsBlacklistArray.length == 1 && (windowsBlacklistArray[0] === "CODE" || windowsBlacklistArray[0] === "VSCODE")) {
+        windowsBlacklistString = '"CODE",';
+    }
+
+    await configs.update("windowsBlacklist", windowsBlacklistString, vscode.ConfigurationTarget.Global);
+
+    globalContext = context;
+
+    fs.readFile("./windows_blacklist.json", "utf8", function (err, data) {
+        if (err) {
+            console.warn(err);
+            return { "error": err }; // prettier-ignore
+        }
+        console.log(data);
+        handleDocumentEdit(context, data);
+    });
+
+    track(context, TRACKED_ACTIONS.ACTIVATE, {
+        at: new Date().toUTCString()
+    });
 }
 
 // this method is called when your extension is deactivated
 function deactivate() {
-  track(globalContext, TRACKED_ACTIONS.DEACTIVATED);
+    track(globalContext, TRACKED_ACTIONS.DEACTIVATED);
 }
 
 module.exports = {
-  activate,
-  deactivate,
+    activate,
+    deactivate
 };
--- src/documentManager.js
+++ src/documentManager.js
@@ -1,50 +1,91 @@
 const vscode = require("vscode");
 const _ = require("lodash");
 const { save } = require("./save");
+const monitor = require("node-active-window");
 
-const justHitEnter = (contentChanges) =>
-  !contentChanges.length ||
-  (contentChanges[0].text.trim() === "" && contentChanges[0].text !== "");
+let activeWin = {};
+let notInCode = false;
+let codeWin = {};
+
+const justHitEnter = (contentChanges) => !contentChanges.length || (contentChanges[0].text.trim() === "" && contentChanges[0].text !== "");
+
+const debounceInMs = vscode.workspace.getConfiguration("cau-vscode-autosave").get("debounceMs");
+const debounceInMsMinus150 = debounceInMs - 150;
+
+setInterval(() => {
+    monitor.getActiveWindow((err, window) => {
+        if (!err && JSON.stringify(activeWin) !== JSON.stringify(window)) {
+            activeWin = window;
+            if (window.app == "Code" && JSON.stringify(codeWin) !== JSON.stringify(window)) {
+                codeWin = window;
+            }
+        }
+    });
+}, debounceInMsMinus150);
+
+let ifOnWindowChangeBool = false;
+
+setInterval(() => {
+    if (vscode.workspace.getConfiguration("cau-vscode-autosave").get("onWindowChanged") !== ifOnWindowChangeBool) {
+        ifOnWindowChangeBool = vscode.workspace.getConfiguration("cau-vscode-autosave").get("onWindowChanged");
+    }
+}, debounceInMsMinus150);
 
 const shouldSkipSave = (e) => {
-  const { contentChanges, document } = e;
-  if (document.isUntitled) return true;
-  if (justHitEnter(contentChanges)) return true;
-  if (hasErrors(document) && isRelevantError(document)) return true;
-  return false;
+    const { contentChanges, document } = e;
+    if (document.isUntitled) return true;
+    if (justHitEnter(contentChanges)) return true;
+    if (hasErrors(document) && isRelevantError(document)) return true;
+    return false;
 };
 
 const getLowestSeverity = (document) => {
-  const diagnostics = vscode.languages.getDiagnostics(document.uri);
-  const errorSeverities = diagnostics.map(({ severity }) => severity);
-  errorSeverities.sort((a, b) => a - b);
-  const [lowestSeverity] = errorSeverities;
-  return lowestSeverity;
+    const diagnostics = vscode.languages.getDiagnostics(document.uri);
+    const errorSeverities = diagnostics.map(({ severity }) => severity);
+    errorSeverities.sort((a, b) => a - b);
+    const [lowestSeverity] = errorSeverities;
+    return lowestSeverity;
 };
 
 const isRelevantError = (document) => {
-  const lowestSeverity = getLowestSeverity(document);
-  return lowestSeverity < 2;
+    const lowestSeverity = getLowestSeverity(document);
+    return lowestSeverity < 2;
 };
 
 const hasErrors = (document) => {
-  const diagnostics = vscode.languages.getDiagnostics(document.uri);
-  return Boolean(diagnostics.length);
+    const diagnostics = vscode.languages.getDiagnostics(document.uri);
+    return Boolean(diagnostics.length);
 };
 
-const handleDocumentEdit = (context) => {
-  const debounceInMs = vscode.workspace
-    .getConfiguration("vscode-autosave")
-    .get("debounceMs");
+const handleDocumentEdit = (context, data = "[]") => {
+    if (!data.startsWith("[")) {
+        throw new TypeError("the parsed data is not a JSON'd array.");
+    }
 
-  vscode.workspace.onDidChangeTextDocument(
-    _.debounce(async (e) => {
-      if (shouldSkipSave(e)) return;
-      await save(context, e.document);
-    }, debounceInMs)
-  );
+    let windowsBlacklistArray = JSON.parse(data);
+
+    if (ifOnWindowChangeBool) {
+        let listener = (winStateE) => {
+            notInCode = !winStateE.focused;
+            if (winStateE.focused == false) {
+                _.debounce(async (e) => {
+                    if (shouldSkipSave(e) || windowsBlacklistArray.indexOf(activeWin.app.toUpperCase()) !== -1) return;
+                    await save(context, e.document);
+                }, debounceInMs);
+            }
+        };
+
+        vscode.window.onDidChangeWindowState(listener);
+    } else {
+        vscode.workspace.onDidChangeTextDocument(
+            _.debounce(async (e) => {
+                if (shouldSkipSave(e)) return;
+                await save(context, e.document);
+            }, debounceInMs)
+        );
+    }
 };
 
 module.exports = {
-  handleDocumentEdit,
+    handleDocumentEdit
 };
